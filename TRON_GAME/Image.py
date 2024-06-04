7ccaf508e3d155dc76162a55bd43134719272d61
Basar <umbunalan@gmail.com>
2024-06-04 16:10:49 +0300

    final version

--- TRON_GAME/Image.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-# Görüntüyü aç
-image = Image.open("match_history.png")
-
-# Görüntüyü GIF formatına dönüştür
-new_image = image.resize((50, 70))
-image.save("Documents/match_history.gif")
